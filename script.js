85388d2dc8cae40b03e8efed7fff62c9ea046d7d
Galdersa <galdersa@gmail.com>
2024-01-17 13:39:14 +0100

    cambio de js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function validateForm() {
     let x = document.forms["myForm"]["fname"].value;
-    let text=x.toLowerCase();
-    if (text == "no te rindas") {
+    /* let text=x.toLowerCase(); */
+    if (x == "No te rindas") {
       alert("¡Respuesta correcta! ¡Felicidades!");
       return true;
     }
-    else (x != "no te rindas"); {
+    else (x != "No te rindas"); {
         alert("Respuesta incorrecta. Prueba otra vez.");
         return false;
     }
