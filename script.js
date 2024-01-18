82298d632f6202794600b472a10f258dbddc17d1
Galdersa <galdersa@gmail.com>
2024-01-18 10:39:32 +0100

    Se cambia el idioma

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function validateForm() {
     let x = document.forms["myForm"]["fname"].value;
     /* let text=x.toLowerCase(); */
     if (x == "No te rindas") {
-      alert("¡Respuesta correcta! ¡Felicidades!");
+      alert("¡Respuesta correcta! ¡Felicidades!");      
       return true;
     }
     else (x != "No te rindas"); {
